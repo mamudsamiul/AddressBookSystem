9b3a52318ceed60cf9f53c0a0eac70cf86fa57e1
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-20 15:03:29 +0530

    (Samiul Mamud) created: Address Book System is created

---
+++ src/main/java/com/capgemini/addressbook/Executer.java
@@ -0,0 +1,8 @@
+package com.capgemini.addressbook;
+
+public class Executer {
+    public static void main(String args[]) {
+    	System.out.println("!! Welcome to Address Book System !!");
+    	System.out.println("=====================================");
+    }
+}
